7a905099e03a24f2189c50aeff675fdc36b7d115
Kunaal Thanik <kunaal@gmx.com>
2024-09-15 19:21:20 -0500

    restructured isolated true and false tests

--- tests/test_string_problems/test_palindrome_string.py
+++ tests/test_string_problems/test_palindrome_string.py
@@ -2,21 +2,35 @@ import pytest
 import string_problems.palindrome_string as ps
 
 
-@pytest.mark.parametrize("test_input, expected_output", [
-    ("civic", True),                  # Single-word palindrome
-    ("hello", False),                 # Non-palindrome single word
-    ("123hello321", False),           # Non-palindrome with numbers
-    ("!@#Civic!@#", False),            # Palindrome with special characters
-    ("", True),                       # Empty string
-    ("a", True),                      # Single character
-    ("12321", True),                  # Numeric palindrome
-    ("12345", False)                  # Non-palindrome numeric string
-])
 class TestPalindrome:
-    def test_is_palindrom(self, test_input, expected_output):
+    @pytest.mark.parametrize("test_input, expected_output", [
+        ("civic", True),                  # Single-word palindrome
+        ("", True),                       # Empty string
+        ("a", True),                      # Single character
+        ("12321", True),                  # Numeric palindrome
+    ])
+    def test_is_palindrom_expected_true(self, test_input, expected_output):
+        """ Test is_palindrome for TRUE values only
+
+        Args:
+            test_input (_type_): value to be tested
+            expected_output (_type_): expected respnse from function
+        """        
         response = ps.PalindromeString(test_input)
         assert response.is_palindrome() == expected_output
     
-    def is_palindrome_optimized(self, test_input, expected_output):
+    @pytest.mark.parametrize("test_input, expected_output", [
+        ("hello", False),                 # Non-palindrome single word
+        ("123hello321", False),           # Non-palindrome with numbers
+        ("!@#Civic!@#", False),            # Palindrome with special characters
+        ("12345", False)                  # Non-palindrome numeric string
+    ])
+    def test_is_palindrom_expected_false(self, test_input, expected_output):
+        """ Test is_palindrome for FALSE values only
+
+        Args:
+            test_input (_type_): value to be tested
+            expected_output (_type_): expected respnse from function
+        """  
         response = ps.PalindromeString(test_input)
         assert response.is_palindrome() == expected_output
